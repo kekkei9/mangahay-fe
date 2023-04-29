7717bffcf3c956235525ddc5824f4296f0fef36d
trongnhandev1011 <kekkei2002@gmail.com>
2023-04-29 23:16:28 +0700

    Revert "nextauth setup"
    This reverts commit 4878d689dafe3024b0dd2c966b631105c0f89210.

--- src/pages/api/auth/[...nextauth].ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import NextAuth from 'next-auth';
-import authOptions from '@/lib/nextAuthOptions';
-
-export default async function auth(req: NextApiRequest, res: NextApiResponse) {
-	return await NextAuth(req, res, authOptions);
-}
