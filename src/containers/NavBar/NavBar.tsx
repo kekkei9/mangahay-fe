bd60533ae1892e2a2e47d71c295f79edd0b83a6d
Trần Nguyễn Quy <kekkei2002@gmail.com>
2023-05-25 10:26:05 +0700

    Update NavBar.tsx

--- src/containers/NavBar/NavBar.tsx
+++ src/containers/NavBar/NavBar.tsx
@@ -4,13 +4,14 @@ import { setAuthToken } from "@/services/backend/axiosClient";
 import { useRouter } from "next/router";
 import { Button } from "primereact/button";
 import { Menu } from "primereact/menu";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 const NavBar = () => {
   const router = useRouter();
   const menuRef = useRef<Menu>(null);
   const dispatch = useDispatch();
+  const [isClient, setIsClient] = useState<boolean>(false);
   const { isAuthUser, user } = useSelector(
     (state: any) => state.authentication
   );
@@ -26,32 +27,37 @@ const NavBar = () => {
     }
   };
 
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <div className="h-10 fixed top-0 left-0 z-50 ">
       <div className="flex justify-around w-screen px-4">
         <div>Just a simple navbar</div>
-        {isAuthUser ? (
-          <>
-            <Button
-              label={`Hi, ${user.fullname}`}
-              onClick={(e) => menuRef.current?.toggle(e)}
-            />
-            <Menu
-              popup
-              ref={menuRef}
-              className="!border-0"
-              model={[
-                {
-                  label: "My account",
-                  command: () => router.push("/auth/account"),
-                },
-                { label: "Log out", command: handleSignOut },
-              ]}
-            />
-          </>
-        ) : (
-          <Button onClick={() => router.push("/auth/signin")}>Log in</Button>
-        )}
+        {isClient &&
+          (isAuthUser ? (
+            <>
+              <Button
+                label={`Hi, ${user.fullname}`}
+                onClick={(e) => menuRef.current?.toggle(e)}
+              />
+              <Menu
+                popup
+                ref={menuRef}
+                className="!border-0"
+                model={[
+                  {
+                    label: "My account",
+                    command: () => router.push("/auth/account"),
+                  },
+                  { label: "Log out", command: handleSignOut },
+                ]}
+              />
+            </>
+          ) : (
+            <Button onClick={() => router.push("/auth/signin")}>Log in</Button>
+          ))}
       </div>
     </div>
   );
