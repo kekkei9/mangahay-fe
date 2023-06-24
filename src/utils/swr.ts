d92afe5f65044f0d72f23f3a8b2f16dd8238bde9
trongnhandev1011 <kekkei2002@gmail.com>
2023-06-24 23:31:10 +0700

    update length

--- src/utils/swr.ts
+++ src/utils/swr.ts
@@ -2,4 +2,4 @@ import { Response } from "@/types/Response.type";
 import { SWRInfiniteResponse } from "swr/infinite";
 
 export const isEmptySWR = (swr: SWRInfiniteResponse<Response<any[]>, any>) =>
-  !swr.data?.[0].result && !swr.isLoading;
+  !swr.data?.[0].result?.length && !swr.isLoading;
